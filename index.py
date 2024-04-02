dc722fb1c320d7bd36cbd4908fa6f9ad47410e2f
parkson-tano <tanoparksonsilencer@gmail.com>
2024-04-02 05:19:31 +0100

    refactor code

--- index.py
+++ index.py
@@ -4,7 +4,6 @@ users_accounts = {"1": {"GBP": 100}, "2": {"EUR": 100}}
 conversion_rate = {("GBP", "EUR"): 1.17}
 
 
-
 class UserNotFoundException(Exception):
     pass
 
@@ -12,75 +11,63 @@ class UserNotFoundException(Exception):
 class SameAccountException(Exception):
     pass
 
+
 class NotEnoughFundsException(Exception):
     pass
 
+
 class CurrencyConvertionRateNotFoundException(Exception):
     pass
+
 
 class MoneyTransfer:
     def __init__(self, user_accounts=None, conversion_rates=None):
         self.users_accounts = users_accounts if users_accounts is not None else {}
         self.conversion_rates = conversion_rate if conversion_rate is not None else {}
 
-
-    def transfer_money(self, sender_id, receiver_id, amount, sender_currency, receiver_currency):
+    def transfer_money(
+        self, sender_id, receiver_id, amount, sender_currency, receiver_currency
+    ):
         if sender_id not in self.users_accounts:
             raise UserNotFoundException(f"Sender {sender_id} not found")
-        
+
         if receiver_id not in self.users_accounts:
             raise UserNotFoundException(f"Receiver {receiver_id} not found")
-        
+
         if sender_id == receiver_id:
             raise SameAccountException(f"Can not send to same account")
-        
 
         if sender_currency in self.users_accounts[sender_id]:
             if self.users_accounts[sender_id][sender_currency] < amount:
                 raise NotEnoughFundsException("Sender does not have enough funds")
-            
-        else:
-            raise NotEnoughFundsException("Sender does not have enough funds")
-        
 
-        # if sender_currency != receiver_currency:
-        #     conversion_key = (sender_currency, receiver_currency)
 
-        #     if conversion_key in conversion_rate:
-        #         converted_amount = amount * self.conversion_rate[conversion_key]
-
-        #     else:
-        #         raise ValueError(f"Conversion rate not available for {sender_currency} to {receiver_currency} ")
-            
-        # else:
-        #     converted_amount =  amount
-
-        converted_amount = self._convert_currency(amount, sender_currency, receiver_currency)
-
+        converted_amount = self._convert_currency(
+            amount, sender_currency, receiver_currency
+        )
         users_accounts[sender_id][sender_currency] -= amount
-        # self.users_accounts[receiver_id][receiver_currency] += converted_amount
 
         if receiver_currency in self.users_accounts[receiver_id]:
             users_accounts[receiver_id][receiver_currency] += converted_amount
-
         else:
             users_accounts[receiver_id][receiver_currency] = converted_amount
 
 
     def _convert_currency(self, amount, from_currency, to_currency):
+        conversion_key = (from_currency, to_currency)
         if from_currency == to_currency:
             return amount
-        conversion_key = (from_currency, to_currency)
-
-        if conversion_key in conversion_rate:
+        try:
             return amount * conversion_rate[conversion_key]
+        except:
+            raise CurrencyConvertionRateNotFoundException(
+                f"Conversion rate not available for {from_currency} to {to_currency}"
+            )
 
-        raise CurrencyConvertionRateNotFoundException(f'Conversion rate not available for {from_currency} to {to_currency}')
-        
 
-
-
-moneyTransfer = MoneyTransfer(user_accounts=users_accounts, conversion_rates=conversion_rate)
+moneyTransfer = MoneyTransfer(
+    user_accounts=users_accounts, conversion_rates=conversion_rate
+)
 
 
 class TestSuite(unittest.TestCase):
